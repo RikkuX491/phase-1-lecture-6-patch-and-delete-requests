2f51a5371ab876d556a7f96cd9ad33ec97f9dfa7
Ricardo Guerra <RikkuG491@gmail.com>
2023-04-17 14:40:19 -0400

    Added solution code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+const foodIDElement = document.getElementById('food-id')
+
 fetch("http://localhost:3000/foods")
 .then(response => response.json())
 .then(foods => {
     foods.forEach(food => {
-        addFoodImageToMenu(food)
+        addFoodImageToMenu(foods, food)
+        createOptionElement(food)
     })
 })
 
@@ -12,14 +15,30 @@ fetch("http://localhost:3000/foods/1")
     displayFoodDetails(food)
 })
 
-function addFoodImageToMenu(food){
+function addFoodImageToMenu(foods, food){
     const restaurantMenu = document.getElementById('restaurant-menu')
+    const foodDiv = document.createElement('div')
     const foodImage = document.createElement('img')
     foodImage.src = food.image
     foodImage.addEventListener('click', () => {
         displayFoodDetails(food)
     })
-    restaurantMenu.appendChild(foodImage)
+    const deleteButton = document.createElement('button')
+    deleteButton.textContent = "DELETE"
+    deleteButton.addEventListener('click', () => {
+        fetch(`http://localhost:3000/foods/${food.id}`, {
+            method: "DELETE"
+        })
+        .then(() => {
+            foods = foods.filter(f => {
+                return f.id !== food.id
+            })
+        })
+    })
+    foodDiv.appendChild(foodImage)
+    foodDiv.appendChild(deleteButton)
+    restaurantMenu.appendChild(foodDiv)
+
 }
 
 function displayFoodDetails(food){
@@ -32,3 +51,53 @@ function displayFoodDetails(food){
     const foodDescription = document.querySelector('#description-display')
     foodDescription.textContent = food.description
 }
+
+function createOptionElement(food){
+    const optionElement = document.createElement('option')
+    optionElement.value = food.id
+    optionElement.textContent = `${food.id}: ${food.name}`
+    foodIDElement.appendChild(optionElement)
+}
+
+// Start coding here
+// foodIDElement.addEventListener('change', (event) => {
+//     console.log(event.target.value)
+// })
+
+const updateFoodForm = document.getElementById('update-food-form')
+updateFoodForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    
+    const foodNameValue = document.getElementById('new-name').value
+    const foodImageValue = document.getElementById('new-image').value
+    const foodDescriptionValue = document.getElementById('new-description').value
+
+    let updatedFood = {}
+    if(foodNameValue !== ""){
+        updatedFood = {...updatedFood, name: foodNameValue}
+    }
+    if(foodImageValue !== ""){
+        updatedFood = {...updatedFood, image: foodImageValue}
+    }
+    if(foodDescriptionValue !== ""){
+        updatedFood = {...updatedFood, description: foodDescriptionValue}
+    }
+
+    let counter = 0
+    for(key in updatedFood){
+        counter++
+    }
+    if(counter > 0){
+        fetch(`http://localhost:3000/foods/${foodIDElement.value}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(updatedFood)
+        })
+    }
+    else{
+        alert("There is nothing to update. Please enter some values.")
+    }
+})
